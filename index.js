5e0e1b2fe11649b502ee211af1f3fef3a2c05ca6
rOBingood7 <mirkomilmuhtorov22@gmail.com>
2024-04-09 13:44:20 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,35 @@
+// const arr = [1,2,3, 'Alex ', 'I am ', [1,2, 'Hello ' ], '24 ', 'My name is ', [1,['years old ', 'and ']]]
+
+// let txt = arr[5][2] + arr[7] + arr[3] + arr[8][1][1] + arr[4] + arr[6] + arr[8][1][0]
+
+// console.log(txt);
+
+
+
+
+
+// const students = ['ilhom', 'xondamir', 'mirko', 'umar', 'ansor', 'muhammadAziz', 'sherobod']
+
+// let n = +prompt('any num')
+// if(n >= 0 && n < students.length) {
+//     students.splice(n, 1)
+// } else {
+//     alert('No student with such number')
+// }
+
+// console.log(students)
+
+
+
+
+// const students = ['ilhom', 'xondamir', 'mirko', 'umar', 'ansor', 'muhammadAziz', 'sherobod']
+
+// let name = prompt('any name').toLowerCase().trim()
+
+// if(students.includes(name)) {
+//     students.splice(students.indexOf(name), 1)
+// } else {
+//     alert('No student with such name')
+// }
+
+// console.log(students)
